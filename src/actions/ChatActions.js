40c8e0bf1380b88ab8567d1f4d61f70252d8794b
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-05-03 22:27:32 -0300

    Criada a ação para setar a lista de usuários

--- src/actions/ChatActions.js
+++ src/actions/ChatActions.js
@@ -1,6 +1,29 @@
 import firebase from '../FirebaseCon';
 
 
+//Pegando lista de contatos
+export const getContactList = () => {
+  return (dispatch) => {
+    firebase.database().ref('users').once('value').then((snapshot)=>{
+
+      let users = [];
+      snapshot.forEach((childItem)=>{
+        users.push({
+          key:childItem.key,
+          name: childItem.val().name
+        });
+      });
+
+      dispatch({
+        type: 'setContactList',
+        payload:{
+          users:users
+        }
+      });
+
+    });
+  };
+};
 
 
 
